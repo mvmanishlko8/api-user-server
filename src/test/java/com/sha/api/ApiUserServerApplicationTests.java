c75b978f01030da5f13fd36dc9c39a73b58c516d
Manish Verma <manishverma@Manishs-iMac.local>
2023-03-22 21:51:31 +0530

    Initial Files

--- src/test/java/com/sha/api/ApiUserServerApplicationTests.java
+++ src/test/java/com/sha/api/ApiUserServerApplicationTests.java
@@ -1,9 +1,10 @@
 package com.sha.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ServletWebServerFactoryAutoConfiguration.class)
 class ApiUserServerApplicationTests {
 
 	@Test
