956043a1b9303824333126ddb0e594912f4aeb92
Manish Verma <manishverma@Manishs-iMac.local>
2023-03-22 20:09:44 +0530

    Initial Files

---
+++ src/main/java/com/sha/api/ApiUserServerApplication.java
@@ -0,0 +1,13 @@
+package com.sha.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUserServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUserServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sha/api/ApiUserServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sha.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUserServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
